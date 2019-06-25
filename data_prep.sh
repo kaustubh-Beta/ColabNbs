f7f45bf84786c464964da320f20c439a349c99fb
kaustubh-Beta <46396273+kaustubh-Beta@users.noreply.github.com>
2019-06-26 04:56:27 +0530

    Add files via upload

---
+++ data_prep.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+mkdir data
+
+mkdir -p "data/train"
+mkdir -p "data/test"
+mkdir -p "data/valid"
+
+for class_F in "$@"
+do
+  mkdir -p "data/train/$class_F";
+  mkdir -p "data/test/$class_F";
+  mkdir -p "data/valid/$class_F";
+
+  cp 101_ObjectCategories/$class_F/image_00[0-5][0-9].jpg data/train/$class_F/
+  cp 101_ObjectCategories/$class_F/image_0060.jpg data/train/$class_F/
+  cp 101_ObjectCategories/$class_F/image_006[1-9].jpg data/valid/$class_F/
+  cp 101_ObjectCategories/$class_F/image_0070.jpg data/valid/$class_F/
+  cp 101_ObjectCategories/$class_F/image_00[7-9]* data/test/$class_F/
+done
+
+echo Done Bro !
