15a84bce24fe869adaebdd3d26a1e656550869dc
kaustubh-Beta <46396273+kaustubh-Beta@users.noreply.github.com>
2019-06-27 00:49:29 +0530

    Update data_prep.sh

--- PytorchTransferLearning/data_prep.sh
+++ PytorchTransferLearning/data_prep.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+echo preparing dataset .......
 mkdir data
 
 mkdir -p "data/train"
 mkdir -p "data/test"
 mkdir -p "data/valid"
 
+echo created training data folder ....
+echo created test data folder ....
+echo created validation data folder ....
 for class_F in "$@"
 do
   mkdir -p "data/train/$class_F";
@@ -19,4 +23,4 @@ do
   cp 101_ObjectCategories/$class_F/image_00[7-9]* data/test/$class_F/
 done
 
-echo Done Bro !
+echo process completed !
